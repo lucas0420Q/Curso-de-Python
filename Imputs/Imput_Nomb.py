51cfa6b5bbefe9f6a70067443de5cfe05e0ba2ff
lucas_zaracho <lucas.zaracho@clt.com.py>
2025-05-19 10:28:23 -0300

    Agregar scripts para solicitar nombre y número al usuario

---
+++ Imputs/Imput_Nomb.py
@@ -0,0 +1,5 @@
+
+# Pedirle un dato al usuario para que se guarde en una variable. Siempre devuelve un string
+nombre = input("Ingrese tu nombre: ")
+
+print("Hola " + nombre + ", bienvenido al programa de Python")
---
+++ Imputs/Imput_nume.py
@@ -0,0 +1,8 @@
+
+# Pedirle un numero al usuario para que se guarde en una variable. Siempre devuelve un string
+numero = input("Ingrese un numero: ")
+
+# Convertir el string a un entero,Multiplicar el numero por 2
+resultado = int(numero) * 2
+
+print("El doble de tu numero es: " + str(resultado))
