a2b6cace9f4fd185b4229a8678617932326f6ae8
lucas_zaracho <lucas.zaracho@clt.com.py>
2025-05-23 16:37:52 -0300

    Agregar script de ejemplo de bucle while en Python

---
+++ Bucles/bucle_while.py
@@ -0,0 +1,9 @@
+
+contador = 0
+
+# El bucle while se ejecuta mientras la condición sea verdadera
+while contador < 10:
+    contador += 1
+    print(f'El contador es {contador}')
+
+print('Fin del bucle while')
